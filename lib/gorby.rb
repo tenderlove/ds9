cd60801d53f407f110e4e3bc18f200961d05f71a
Aaron Patterson <aaron.patterson@gmail.com>
2015-06-26 17:14:53 -0700

    subclass not delegate

--- lib/gorby.rb
+++ lib/gorby.rb
@@ -24,7 +24,18 @@ module Gorby
     end
   end
 
-  class Events
+  class Session
+    def initialize
+      cbs = make_callbacks callbacks
+      init_internals cbs
+    end
+
+    private
+
+    def callbacks
+      self.class.instance_methods(false).grep(/^on_|event$/)
+    end
+
     def send_event string
       string.length
     end
@@ -60,68 +71,6 @@ module Gorby
     end
   end
 
-  class Session
-    attr_reader :target
-
-    def initialize target
-      @target = target
-      cbs = make_callbacks callbacks
-      init_internals cbs
-    end
-
-    private
-
-    def callbacks
-      CALLBACKS
-    end
-
-    def send_event string
-      @target.send_event string
-    end
-
-    def on_frame_recv frame
-      @target.on_frame_recv frame
-    end
-
-    def on_stream_close id, error_code
-      @target.on_stream_close id, error_code
-    end
-
-    def on_header name, value, frame, flags
-      @target.on_header name, value, frame, flags
-    end
-
-    def on_begin_headers frame
-      @target.on_begin_headers frame
-    end
-
-    def recv_event length
-      @target.recv_event length
-    end
-
-    def on_begin_frame frame
-      @target.on_begin_frame frame
-    end
-
-    def on_data_chunk_recv id, data, flags
-      @target.on_data_chunk_recv id, data, flags
-    end
-
-    def on_invalid_frame_recv frame, error_code
-      @target.on_invalid_frame_recv frame, error_code
-    end
-
-    def on_frame_send frame
-      @target.on_frame_send frame
-    end
-
-    def on_frame_not_send frame, reason
-      @target.on_frame_not_send frame, reason
-    end
-
-    CALLBACKS = private_instance_methods(false).find_all { |m|
-      m =~ /^on_|event$/
-    }
-
+  class Client < Session
   end
 end
--- test/test_gorby.rb
+++ test/test_gorby.rb
@@ -7,17 +7,17 @@ class TestGorby < Minitest::Test
   end
 
   def test_session_allocation
-    assert Gorby::Session.new Gorby::Events.new
+    assert Gorby::Session.new
   end
 
   def test_session_submit_settings
-    session = Gorby::Session.new Gorby::Events.new
+    session = Gorby::Session.new
     session.submit_settings [[Gorby::Settings::MAX_CONCURRENT_STREAMS, 100]]
   end
 
   def test_send
     called = false
-    session = Gorby::Session.new Class.new(Gorby::Events) {
+    session = Class.new(Gorby::Session) {
       define_method :send_event do |string|
         called = string
         super(string)
@@ -30,17 +30,17 @@ class TestGorby < Minitest::Test
   end
 
   def test_want_read?
-    session = Gorby::Session.new Gorby::Events.new
+    session = Gorby::Session.new
     session.submit_settings [[Gorby::Settings::MAX_CONCURRENT_STREAMS, 100]]
     assert_predicate session, :want_write?
   end
 
   def test_want_write?
-    session = Gorby::Session.new Gorby::Events.new
+    session = Gorby::Session.new
     session.submit_settings [[Gorby::Settings::MAX_CONCURRENT_STREAMS, 100]]
     assert_predicate session, :want_write?
 
-    session = Gorby::Session.new Gorby::Events.new
+    session = Gorby::Session.new
     refute_predicate session, :want_write?
   end
 end
