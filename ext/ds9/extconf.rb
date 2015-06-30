2720074d390cb938f983b189a4b82dddaa5d8732
Aaron Patterson <aaron.patterson@gmail.com>
2015-06-30 13:42:50 -0700

    add option for static compilation

--- ext/ds9/extconf.rb
+++ ext/ds9/extconf.rb
@@ -1,5 +1,16 @@
 require 'mkmf'
 
-pkg_config 'libnghttp2'
+if with_config("static")
+  ldflags = pkg_config 'libnghttp2', 'libs-only-L'
+
+  archive = File.join ldflags.sub(/^-L/, ''), 'libnghttp2.a'
+  if File.exist? archive
+    $LDFLAGS << " #{archive}"
+  else
+    raise "couldn't find archive"
+  end
+else
+  pkg_config 'libnghttp2'
+end
 
 create_makefile 'ds9'
